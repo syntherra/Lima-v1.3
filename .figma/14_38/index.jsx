4a79ec4ec33cee3ed7f06fa1449ffc7cd31b2ab4
thecognifyai <syntellecta@gmail.com>
2025-08-22 23:58:34 +0200

    update

---
+++ .figma/14_38/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import styles from './index.module.scss';
+
+const Component = () => {
+  return (
+    <div className={styles.logInSIgnUpPage}>
+      <div className={styles.frame18}>
+        <div className={styles.frame20}>
+          <img src="../image/menc377z-jfzan55.svg" className={styles.lima} />
+          <p className={styles.aIPoweredGrowthOs}>AI Powered Growth OS</p>
+        </div>
+        <div className={styles.frame22}>
+          <p className={styles.tiredOfSoundingLikeA}>
+            Tired of sounding like a bot? Lima learns how you write and sends emails
+            that feel personal — not templated — at scale.
+            <br />
+          </p>
+          <div className={styles.frame23}>
+            <div className={styles.rectangle1} />
+            <div className={styles.rectangle5} />
+            <div className={styles.rectangle1} />
+            <div className={styles.rectangle1} />
+          </div>
+        </div>
+      </div>
+      <div className={styles.mainDashboardCompone} />
+    </div>
+  );
+}
+
+export default Component;
--- .figma/1_2/index.jsx
+++ .figma/1_2/index.jsx
@@ -7,51 +7,51 @@ const Component = () => {
     <div className={styles.desktop1}>
       <div className={styles.sidebar}>
         <div className={styles.frame1}>
-          <img src="../image/men3b86p-l1dh2c4.svg" className={styles.lima} />
+          <img src="../image/men5ysc3-76ekpni.svg" className={styles.lima} />
           <p className={styles.aIPoweredGrowthOs}>AI Powered Growth OS</p>
         </div>
         <div className={styles.frame2}>
           <div className={styles.frame6}>
-            <img src="../image/men3b86p-4ffclfj.png" className={styles.email} />
+            <img src="../image/men5ysc3-tmdt6wh.png" className={styles.email} />
             <p className={styles.aIPoweredGrowthOs}>Inbox</p>
           </div>
           <div className={styles.frame6}>
-            <img src="../image/men3b86p-0njmn40.png" className={styles.email} />
+            <img src="../image/men5ysc3-f1mi0mo.png" className={styles.email} />
             <p className={styles.aIPoweredGrowthOs}>Deleted Items</p>
           </div>
           <div className={styles.frame6}>
-            <img src="../image/men3b86p-k6f9w87.png" className={styles.email} />
+            <img src="../image/men5ysc3-c9255tp.png" className={styles.email} />
             <p className={styles.aIPoweredGrowthOs}>CRM</p>
           </div>
           <div className={styles.frame6}>
-            <img src="../image/men3b86p-k6f9w87.png" className={styles.email} />
+            <img src="../image/men5ysc3-c9255tp.png" className={styles.email} />
             <p className={styles.aIPoweredGrowthOs}>CRM</p>
           </div>
           <div className={styles.frame6}>
-            <img src="../image/men3b86p-k6f9w87.png" className={styles.email} />
+            <img src="../image/men5ysc3-c9255tp.png" className={styles.email} />
             <p className={styles.aIPoweredGrowthOs}>CRM</p>
           </div>
         </div>
-        <img src="../image/men3b86p-rika21g.svg" className={styles.frame13} />
+        <img src="../image/men5ysc3-m1nuly6.svg" className={styles.frame13} />
         <div className={styles.frame3}>
           <div className={styles.frame9}>
-            <img src="../image/men3b86p-k6f9w87.png" className={styles.email} />
+            <img src="../image/men5ysc3-c9255tp.png" className={styles.email} />
             <p className={styles.aIPoweredGrowthOs}>Mail</p>
           </div>
           <div className={styles.frame6}>
-            <img src="../image/men3b86p-wel2eqq.png" className={styles.email} />
+            <img src="../image/men5ysc3-obvfuv3.png" className={styles.email} />
             <p className={styles.aIPoweredGrowthOs}>Growth OS</p>
           </div>
           <div className={styles.frame6}>
-            <img src="../image/men3b86p-k6f9w87.png" className={styles.email} />
+            <img src="../image/men5ysc3-c9255tp.png" className={styles.email} />
             <p className={styles.aIPoweredGrowthOs}>CRM</p>
           </div>
           <div className={styles.frame6}>
-            <img src="../image/men3b86p-w0mk4e7.png" className={styles.email} />
+            <img src="../image/men5ysc3-7jqyn5r.png" className={styles.email} />
             <p className={styles.aIPoweredGrowthOs}>Calendar</p>
           </div>
           <div className={styles.frame6}>
-            <img src="../image/men3b86p-iqtgqfv.png" className={styles.email} />
+            <img src="../image/men5ysc3-lz453ep.png" className={styles.email} />
             <p className={styles.aIPoweredGrowthOs}>Tasks</p>
           </div>
         </div>
@@ -59,14 +59,14 @@ const Component = () => {
       <div className={styles.mainDashboardCompone}>
         <div className={styles.frame14}>
           <img
-            src="../image/men3b86p-udu6pp1.png"
+            src="../image/men5ysc3-1jx5pyg.png"
             className={styles.crescentMoon}
           />
           <img
-            src="../image/men3b86p-i6d7o6a.png"
+            src="../image/men5ysc3-8uoted9.png"
             className={styles.crescentMoon}
           />
-          <img src="../image/men3b86p-tlpp9sq.svg" className={styles.frame17} />
+          <img src="../image/men5ysc3-l4us79m.svg" className={styles.frame17} />
         </div>
       </div>
     </div>
